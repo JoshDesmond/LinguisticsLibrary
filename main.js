27a9e00b07d98909a18d23cd1c17daae5bca9b19
JoshDesmond <JoshDesmond@users.noreply.github.com>
2022-05-14 11:45:08 -0400

    Reworks template mongodb script

--- main.js
+++ main.js
@@ -1,40 +1,28 @@
-/*
- * This is a temporary reference file based on a tutorial from:
- * https://attacomsian.com/blog/nodejs-mongodb-local-connection
- */ 
 const MongoClient = require('mongodb').MongoClient;
 
-// Connect URL
 const url = 'mongodb://127.0.0.1:27017';
-
-// Connec to MongoDB
-MongoClient.connect(url, {
+const options = {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
-}, (err, client) => {
-	if (err) {
-		return console.log(err);
+};
+
+const words = ["hello", "hi", "howdy"];
+
+MongoClient.connect(url, options, (err, client) => {
+	if (err) { return console.error(err); }
+
+	const db = client.db('words');
+
+	const collection = db.collection('gloveWords');
+
+	for (const word of words) {
+		collection.insertOne({ name: word}, (err, result) => {
+			console.error(err);
+		});
 	}
 
-	// Specify database you want to access
-	const db = client.db('school');
-
-
-	const courses = db.collection('courses');
-
-	courses.insertOne({ name: 'Web Security' }, (err, result) => { });
-
-	courses.insertMany([
-		{ name: 'Web Design' },
-		{ name: 'Distributed Database' },
-		{ name: 'Artificial Intelligence' }
-	], (err, results) => {
-		console.error(err);
-	});
-
-	courses.find().toArray((err, results) => {
+	collection.find().toArray((err, results) => {
 		console.log(results);
 	});
-	console.log(`MongoDB Connected: ${url}`);
 });
 
